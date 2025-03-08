13d2bdb0c9a309d340694adabfabaa0c5c815c06
Rafael Baptista <rafael.sampaio@aluno.faculdadeimpacta.com.br>
2025-03-08 14:49:27 -0300

    fix: home

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -10,7 +10,7 @@ echo 'installing this dependency, it would typically be done so using this'
 echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
 set -x
-npm install --save-dev vitest @testing-library/react @testing-library/jest-dom typescript ts-jest
+npm install --save-dev vitest
 npm install --save-dev cross-env
 set +x
 
@@ -18,5 +18,4 @@ echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
 set -x
-
 npm test
