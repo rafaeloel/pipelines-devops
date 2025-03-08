a8d8edb5e5e293a736506c35101d04174cfd5530
Rafael Baptista <rafael.sampaio@aluno.faculdadeimpacta.com.br>
2025-03-08 15:01:44 -0300

    feat: ajustes vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Permite o acesso de qualquer IP
+    port: 3000,  // Define a porta para 3000
+  }
 })
+
+
