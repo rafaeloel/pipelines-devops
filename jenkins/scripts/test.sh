e647471324944e2573562c892bafff8d6c2452f2
Rafael Baptista <rafael.sampaio@aluno.faculdadeimpacta.com.br>
2025-03-08 14:41:34 -0300

    fix: cross-env

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -11,6 +11,7 @@ echo 'flag. For a comprehensive explanation about "devDependencies", see'
 echo 'https://stackoverflow.com/questions/18875674/whats-the-difference-between-dependencies-devdependencies-and-peerdependencies.'
 set -x
 npm install --save-dev vitest @testing-library/react @testing-library/jest-dom typescript ts-jest
+npm install --save-dev cross-env
 set +x
 
 echo 'The following "npm" command tests that your simple Node.js/React'
