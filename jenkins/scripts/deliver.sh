09ecfdb3a199e3b28598329eb704879e1514dfe5
Rafael Baptista <rafael.sampaio@aluno.faculdadeimpacta.com.br>
2025-03-08 12:55:12 -0300

    feat: change command

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,14 +11,14 @@ set +x
 
 echo 'The following "npm" command runs your Node.js/React application in'
 echo 'development mode and makes the application available for web browsing.'
-echo 'The "npm start" command has a trailing ampersand so that the command runs'
+echo 'The "npm run dev" command has a trailing ampersand so that the command runs'
 echo 'as a background process (i.e. asynchronously). Otherwise, this command'
-echo 'can pause running builds of CI/CD applications indefinitely. "npm start"'
+echo 'can pause running builds of CI/CD applications indefinitely. "npm run dev"'
 echo 'is followed by another command that retrieves the process ID (PID) value'
-echo 'of the previously run process (i.e. "npm start") and writes this value to'
+echo 'of the previously run process (i.e. "npm run dev") and writes this value to'
 echo 'the file ".pidfile".'
 set -x
-npm start &
+npm run dev &
 sleep 1
 echo $! > .pidfile
 set +x
